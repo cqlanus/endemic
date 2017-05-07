ca6a2a994fd9871b8129ae30bc31fbd411ba53b2
Chris Lanus <cqlanus@gmail.com>
2017-05-07 09:51:45 -0500

    removed logging

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.get('/', (req, res, next) => {
     const indexPath = path.join(__dirname, '..', 'index.html')
     res.sendFile(indexPath);
